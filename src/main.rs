411b922523a32442a02fe0eaa3a0e6a5daccc5a7
Ben Chaimberg <ben.chaimberg@yale.edu>
2021-09-24 00:03:36 -0400

    rename configuration file to convention

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod host;
 mod http;
 
 fn main() -> Result<(), error::Error> {
-    let server_config = config::load_config(&std::path::Path::new("/home/accts/bnc24/cs434/projects/p1/server.conf"))?;
+    let server_config = config::load_config(&std::path::Path::new("/home/accts/bnc24/cs434/projects/p1/httpd.conf"))?;
     let port = server_config.listen_port;
     let listener = std::net::TcpListener::bind(format!("127.0.0.1:{}", port))?;
     println!("Listening on port {}...", port);
