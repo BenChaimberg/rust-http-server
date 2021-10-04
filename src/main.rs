84ca00da838c1cc2cb9dbe1ebcf037574c0456ec
Ben Chaimberg <ben.chaimberg@yale.edu>
2021-10-04 00:44:52 -0400

    take config file as command line argument
    closes #12

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ mod parse;
 mod time;
 
 fn main() -> Result<(), error::Error> {
-    let server_config = config::load_config(&std::path::Path::new("/home/accts/bnc24/cs434/projects/p1/httpd.conf"))?;
+    let args = std::env::args().collect::<Vec<String>>();
+    let config_file_arg = args.get(1).map(|s| s.as_str()).unwrap_or("httpd.conf");
+    let config_file = std::env::current_dir()?.join(config_file_arg);
+    let server_config = config::load_config(&config_file)?;
+
     let port = server_config.directives.get(&config::Directive::ListenPort).ok_or(error::Error::new("Config did not include a ListenPort entry".to_string()))?;
     let listener = std::net::TcpListener::bind(format!("127.0.0.1:{}", port))?;
     println!("Listening on port {}...", port);
@@ -17,7 +21,7 @@ fn main() -> Result<(), error::Error> {
     // accept connections and process them serially
     for stream in listener.incoming() {
         if let Err(e) = http::handle_client(stream?, &handler) {
-            println!("Encounted error: {:#?}", e);
+            println!("Encountered error: {:#?}", e);
         }
     }
     Ok(())
